9d083f1e48018e0887c74ed38a8531a1d1d75b12
StylishGoldPen <stylishgoldpen@yahoo.com.sg>
2016-09-07 11:43:16 +0800

    Add Tagging class

---
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -0,0 +1,31 @@
+package seedu.addressbook.data.tag;
+
+import seedu.addressbook.data.person.Person;
+
+public class Tagging {
+	
+	private Person person;
+	private Tag tag;
+	private String task;
+	
+	public Tagging (Person person, Tag tag, String task) {
+		this.tag = tag;
+		this.person = person;
+		this.task = task;
+	}
+	
+	public String toString(){
+		String operation;
+		
+		if (this.task == "DELETED") {
+			operation = "-";	
+		}
+		else
+		{
+			operation = "+";
+		}
+		
+		return operation + person.getName() + tag.toString();
+		
+	}
+}
